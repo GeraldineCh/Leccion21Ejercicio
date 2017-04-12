d8439e3272c5f4b29c35d89487d263ba9d929fe9
Geraldine Chauca <gchaucae@gmail.com>
2017-04-12 16:48:57 -0500

    Se quiere generar los div coon js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,42 @@
-/*
+//PRODUCTOS
+var projects = ["","TAYMAN GROUP", "NALIA ORGANICS", "BRASS COQ","SUNGENRE","LIVING BOTANICS",
+"FOAM KING","VOLITION","INK","GILKON","OUTS APPLES","SHERWOOD","PLATINUM"];
 
-var modal = document.getElementById('myModal');
+var num=projects.length;
+var productos=document.getElementById('salida');
 
-var img = document.getElementById('img1');
-var modalImg = document.getElementById("img01");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
+function imprimir(event){
+  var productos=document.getElementById('salida');
+
+  for(var i=0; i<num;i++){
+	  var div=document.createElement("div");
+	  div.setAttribute("class","work-products");
+	  
+	  var figure=document.createElement("figure");
+	  var img=document.createElement("img");
+	  img.src="assets/img/catalogo/img-" + (i+1) +".jpg";
+	  img.setAttribute("class","img");
+	  img.alt=projects[i];
+	  var figcaption=document.createElement("figcaption");
+	  var span=document.createElement("span");
+	  span.setAttribute("class","nombre");
+	  span.innerHTML= projects[i];
+
+	  figcaption.appendChild(span);
+	  figure.appendChild(figcaption);
+	  figure.appendChild(img);
+	  div.appendChild(figure);
+  }
 }
+window.addEventListener('load', imprimir);
 
-var span = document.getElementsByClassName("close")[0];
-span.onclick = function() { 
-    modal.style.display = "none";}
- */
-var modal = document.getElementById('myModal');
+
+//MODAL
+
+var modal = document.getElementById('pop');
 
 var img = document.getElementsByClassName('img');
-var modalImg = document.getElementById("img01");
+var modalImg = document.getElementById("salida");
 for (var i = 0; i < img.length; i++) {
 	img[i].onclick = function(){
     modal.style.display = "block";
